4ccee18e2e0e3bbfb6b7733216372a6c89f4c199
liyi <liyu283026@163.com>
2018-09-03 14:09:30 +0800

    add gif

--- LYRingChartViewDemo/ViewController.swift
+++ LYRingChartViewDemo/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var ring:TDRingChartView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ring = TDRingChartView(frame: CGRect(x: (self.view.frame.width-150)/2, y: 150, width: 150, height: 150), percentArr: [0.25,0.25,0.25,0.25], colorArr: [UIColor.yellow ,UIColor.red,UIColor.cyan,UIColor.brown], durtion: 0.5)
+      let  ring = TDRingChartView(frame: CGRect(x: (self.view.frame.width-150)/2, y: 150, width: 150, height: 150), percentArr: [0.25,0.25,0.25,0.25], colorArr: [UIColor.yellow ,UIColor.red,UIColor.cyan,UIColor.brown], durtion: 0.5)
         ring.titleArr =  ["可用余额","我的理财","我的资产","我的遗产"]
         self.view.addSubview(ring)
     }
